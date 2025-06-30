eef2e26f038f27bb0e89e24ae21cd7c389a5544b
Tesa44 <mateusz.tesarewicz123@gmail.com>
2025-06-30 18:41:02 +0200

    clean code

--- src/components/App.js
+++ src/components/App.js
@@ -11,83 +11,6 @@ import Loader from "./Loader";
 import ErrorMessage from "./ErrorMessage";
 import PaginationBar from "./PaginationBar";
 
-const tempCharacters = [
-  {
-    id: 1,
-    name: "Rick Sanchez",
-    status: "Alive",
-    species: "Human",
-    type: "",
-    gender: "Male",
-    origin: {
-      name: "Earth (C-137)",
-      url: "https://rickandmortyapi.com/api/location/1",
-    },
-    location: {
-      name: "Earth (Replacement Dimension)",
-      url: "https://rickandmortyapi.com/api/location/20",
-    },
-    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-    episode: [
-      "https://rickandmortyapi.com/api/episode/1",
-      "https://rickandmortyapi.com/api/episode/2",
-      // ...
-    ],
-    url: "https://rickandmortyapi.com/api/character/1",
-    created: "2017-11-04T18:48:46.250Z",
-  },
-  {
-    id: 2,
-    name: "Morty Smith",
-    status: "Alive",
-    species: "Human",
-    type: "",
-    gender: "Male",
-    origin: {
-      name: "Earth",
-      url: "https://rickandmortyapi.com/api/location/1",
-    },
-    location: {
-      name: "Earth",
-      url: "https://rickandmortyapi.com/api/location/20",
-    },
-    image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
-    episode: [
-      "https://rickandmortyapi.com/api/episode/1",
-      "https://rickandmortyapi.com/api/episode/2",
-      // ...
-    ],
-    url: "https://rickandmortyapi.com/api/character/2",
-    created: "2017-11-04T18:50:21.651Z",
-  },
-  {
-    id: 183,
-    name: "Johnny Depp",
-    status: "Alive",
-    species: "Human",
-    type: "",
-    gender: "Male",
-    origin: {
-      name: "Earth (C-500A)",
-      url: "https://rickandmortyapi.com/api/location/23",
-    },
-    location: {
-      name: "Earth (C-500A)",
-      url: "https://rickandmortyapi.com/api/location/23",
-    },
-    image: "https://rickandmortyapi.com/api/character/avatar/183.jpeg",
-    episode: ["https://rickandmortyapi.com/api/episode/8"],
-    url: "https://rickandmortyapi.com/api/character/183",
-    created: "2017-12-29T18:51:29.693Z",
-  },
-];
-
-const PAGES = 42;
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
 export default function App() {
   const [characters, setCharacters] = useState([]);
   const [selectedCharacter, setSelectedCharacter] = useState("");
@@ -98,13 +21,8 @@ export default function App() {
   const [pages, setPages] = useState(1);
   const [numResults, setNumResults] = useState("");
 
-  function handleSelectCharacter(character) {
-    setSelectedCharacter(character);
-  }
-
   useEffect(
     function () {
-      const randomPage = getRandomInt(PAGES - 1) + 1;
       const controller = new AbortController();
       async function fetchCharacters() {
         try {
@@ -200,7 +118,7 @@ export default function App() {
             <>
               <CharacterList
                 characters={characters}
-                onSelectCharacter={handleSelectCharacter}
+                onSelectCharacter={setSelectedCharacter}
               ></CharacterList>
               <PaginationBar
                 pages={pages}
